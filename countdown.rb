7e35936caa464d7ed8387809d0465d992440eefa
icika-max <icikakennedy@gmail.com>
2023-02-09 18:50:13 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+number=12
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  # puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (number)
+    while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep 5
+    number -= 1
+  end
+  # puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
